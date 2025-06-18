ede7cb145f572c39608a35e40465ed13041966be
akshaycu <akshaycu9093@gmail.com>
2025-06-18 23:41:23 +0530

    logout-testcase done

--- magento-automation/cypress/e2e/logout.cy.js
+++ magento-automation/cypress/e2e/logout.cy.js
@@ -6,22 +6,18 @@ describe('User Logout', () => {
   const account = new AccountPage();
 
   it('should log out successfully', () => {
-    // Step 1: Login
+
     login.visit();
     login.fillCredentials('yourtestemail@gmail.com', 'Password@123!');
     login.submit();
 
-    // Step 2: Confirm login
     cy.get('.panel.header').should('contain.text', 'Welcome');
 
-    // Step 3: Logout
     account.logout();
 
-    // Step 4: Verify logout success URL
     cy.url().should('include', 'customer/account/logoutSuccess');
 
-    // Step 5: Wait for redirect and confirm homepage
-    cy.wait(6000); // Wait for the auto-redirect
+    cy.wait(5000);
     cy.url().should('eq', 'https://magento.softwaretestingboard.com/');
   });
 });
