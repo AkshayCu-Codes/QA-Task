ed578230241bd731c58f531628419f6c456aa9e2
akshaycu <akshaycu9093@gmail.com>
2025-06-19 12:02:55 +0530

    Updated login test to load email and password from saved fixture

--- magento-automation/cypress/e2e/login.cy.js
+++ magento-automation/cypress/e2e/login.cy.js
@@ -3,11 +3,13 @@ import LoginPage from '../pages/LoginPage';
 describe('User Login', () => {
   const login = new LoginPage();
 
-  it('should login successfully', () => {
-    login.visit();
-    login.fillCredentials('yourtestemail@gmail.com', 'Password@123!');
-    login.submit();
+  it('should login successfully using credentials from fixture', () => {
+    cy.fixture('credentials.json').then((creds) => {
+      login.visit();
+      login.fillCredentials(creds.email, creds.password);
+      login.submit();
 
-    cy.get('.panel.header').should('contain.text', 'Welcome');
+      cy.get('.panel.header').should('contain.text', 'Welcome');
+    });
   });
 });
