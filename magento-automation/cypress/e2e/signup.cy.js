5cdf920aae3e2128efda608563591cfd56896b73
akshaycu <akshaycu9093@gmail.com>
2025-06-19 11:59:35 +0530

    Unique signup mail added for each time.

--- magento-automation/cypress/e2e/signup.cy.js
+++ magento-automation/cypress/e2e/signup.cy.js
@@ -3,16 +3,26 @@ import SignupPage from '../pages/SignupPage';
 describe('User Signup', () => {
   const signup = new SignupPage();
 
-  const emailId = 'yourtestemail@gmail.com';
+  // Generate unique email
+  const timestamp = Date.now();
+  const emailId = `testuser${timestamp}@mailinator.com`;
   const password = 'Password@123!';
 
-  it('should sign up successfully', () => {
+  it('should sign up successfully and save credentials', () => {
     signup.visit();
     signup.fillForm('Test', 'User', emailId, password);
     cy.wait(1000);
     signup.submit();
     cy.wait(1000);
 
-    cy.get('.message-success').should('contain.text', 'Thank you for registering');
+    cy.get('.message-success')
+      .should('contain.text', 'Thank you for registering')
+      .then(() => {
+        // Saveing credentials for future tests
+        cy.writeFile('cypress/fixtures/credentials.json', {
+          email: emailId,
+          password: password
+        });
+      });
   });
 });
